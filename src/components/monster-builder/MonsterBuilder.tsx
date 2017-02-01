a82f85ccfd507b683aabbfae721b155a1190bf9b
Blair Knickerbocker <b.knickerbocker@shieldsafety.co.uk>
2017-02-01 17:07:12 +0000

    * UpDownLinks component done * work toward wireframing the full monster editor

--- src/components/monster-builder/MonsterBuilder.tsx
+++ src/components/monster-builder/MonsterBuilder.tsx
@@ -25,8 +25,14 @@ interface Defenses
     HitDieSize: number;
     HitDiceCount: number;
 
+    ACFormulaType: string;
+
     BaseAC: number;
 }
+
+const STANDARD_ARMOR: "STANDARD_ARMOR" = "STANDARD_ARMOR";
+const NATURAL_ARMOR: "NATURAL_ARMOR" = "NATURAL_ARMOR";
+const UNARMORED_DEFENSE: "UNARMORED_DEFENSE" = "UNARMORED_DEFENSE";
 
 interface MonsterStatsState
 {
@@ -36,7 +42,7 @@ interface MonsterStatsState
 
 const DEFAULT_MONSTER_STATS_STATE: MonsterStatsState = {
     Attributes: {Str: 10, Dex: 10, Con: 10, Int: 10, Wis: 10, Cha: 10,},
-    Defenses: {HitDieSize: 8, HitDiceCount: 2, BaseAC: 10}
+    Defenses: {HitDieSize: 8, HitDiceCount: 2, ACFormulaType: STANDARD_ARMOR, BaseAC: 10}
 };
 
 class MonsterBuilder extends React.Component<MonsterStatsProps, MonsterStatsState>
@@ -51,6 +57,7 @@ class MonsterBuilder extends React.Component<MonsterStatsProps, MonsterStatsStat
         this.handleAttributeChange = this.handleAttributeChange.bind(this);
         this.ModifyHitDiceCount = this.ModifyHitDiceCount.bind(this);
         this.ModifyHitDieSize = this.ModifyHitDieSize.bind(this);
+        this.handleChangeACFormulaType = this.handleChangeACFormulaType.bind(this);
     }
 
 
@@ -136,6 +143,15 @@ class MonsterBuilder extends React.Component<MonsterStatsProps, MonsterStatsStat
         this.setState({Defenses: def} as MonsterStatsState);
     }
 
+    handleChangeACFormulaType(e: any)
+    {
+        const def = this.state.Defenses;
+
+        def.ACFormulaType = e.target.value;
+
+        this.setState({Defenses: def} as MonsterStatsState);
+    }
+
     render()
     {
         const { monsterName } = this.props;
@@ -150,24 +166,6 @@ class MonsterBuilder extends React.Component<MonsterStatsProps, MonsterStatsStat
         let attributes = Object.keys(Attr).map(key =>
             <Attribute key={key} label={key} value={Attr[key]} modifyAttribute={this.ModifyAttribute} setAttribute={this.SetAttribute} />
         );
-
-        let arrowParentStyle = {
-            position: "relative",
-            top: "2",
-            left: "8"
-        };
-
-        let upArrowStyle = {
-            position: "absolute",
-            // right: "-10px",
-            // "margin-left": "10px"
-        };
-
-        let downArrowStyle = {
-            position: "absolute",
-            // left: "-10px",
-            // "margin-right": "10px"
-        }
 
         return (
             <div className="monster-stats">
@@ -196,18 +194,71 @@ class MonsterBuilder extends React.Component<MonsterStatsProps, MonsterStatsStat
                         <h4>Attributes</h4>
                         {attributes}
                     </div>
-                    <div>
-                        <h3>Defensive CR <span style={arrowParentStyle}><i className="fa fa-sort-up" style={upArrowStyle}></i><i className="fa fa-sort-down" style={downArrowStyle}></i></span></h3>
-                        <h4>Hit Dice <UpDownLinks onUpClicked={e => {console.log("Up Clicked")}} onDownClicked={e => {console.log("Down Clicked")}} /></h4>
-                        <div className="hit-dice-box">
-                            <NumberInput min={1} max={40} value={HitDiceCount} onBlur={this.ModifyHitDiceCount} />
-                            d
-                            <NumberInput min={1} max={12} value={HitDieSize} onBlur={this.ModifyHitDieSize} />
+                    <fieldset>
+                        <legend>Defensive CR</legend>
+                        <div className="defensive-cr">
+                            <div className="defensive-cr-details">
+                                <div>
+                                    <h4>Hit Dice <UpDownLinks onUpClicked={e => {console.log("Up Clicked")}} onDownClicked={e => {console.log("Down Clicked")}} /></h4>
+                                    <div className="hit-dice-box">
+                                        <NumberInput min={1} max={40} value={HitDiceCount} onBlur={this.ModifyHitDiceCount} />
+                                        d
+                                        <NumberInput min={1} max={12} value={HitDieSize} onBlur={this.ModifyHitDieSize} />
+                                    </div>
+                                </div>
+                                <div>
+                                    <h4>Armor Class</h4>
+                                    <div className="armor-formula">
+                                        <select value={this.state.Defenses.ACFormulaType} onChange={this.handleChangeACFormulaType}>
+                                            <option value={STANDARD_ARMOR}>Standard Armor</option>
+                                            <option value={NATURAL_ARMOR}>Natural Armor</option>
+                                            <option value={UNARMORED_DEFENSE}>Unarmored Defense</option>
+                                        </select>
+                                        <br />
+                                        <input type="checkbox" /> Include a Shield
+                                        <br />
+                                        <label>Bonus</label>
+                                        <NumberInput value={0} />
+                                        {(this.state.Defenses.ACFormulaType == UNARMORED_DEFENSE ?
+                                            UNARMORED_DEFENSE
+                                        : this.state.Defenses.ACFormulaType == NATURAL_ARMOR ?
+                                            NATURAL_ARMOR
+                                        : STANDARD_ARMOR)}
+                                    </div>
+                                    <div>
+                                        <label>AC</label>
+                                        <span>12</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="defensive-cr-calculations">
+                                <div>
+                                    <h4>HP Average</h4>
+                                    <span>
+                                        {this.HitDiceAverage()} ({HitDiceCount}d{HitDieSize} + {HitDiceCount * ConMod})
+                                    </span>
+                                </div>
+                                <div>
+                                    <h4>Effective AC</h4>
+                                    <span>14</span>
+                                </div>
+                            </div>
+                            <div className="defensive-cr-outcome">
+                                <div>
+                                    <h4>Defensive CR Rating</h4>
+                                    <span>3</span>
+                                </div>
+                                <div>
+                                    <h4>AutoScale!</h4>
+                                    <UpDownLinks size={2} onUpClicked={e => console.log('up clicked')} onDownClicked={e => console.log('down clicked')} />
+                                </div>
+                            </div>
                         </div>
-                        <div>
-                            {this.HitDiceAverage()} ({HitDiceCount}d{HitDieSize} + {HitDiceCount * ConMod})
-                        </div>
-                    </div>
+                    </fieldset>
+                    <fieldset className="offensive-cr">
+                        <legend>Offensive CR</legend>
+                        <UpDownLinks size={2} onUpClicked={e => {console.log("Up Clicked")}} onDownClicked={e => {console.log("Down Clicked")}}  />
+                    </fieldset>
                 </fieldset>
             </div>
         );
