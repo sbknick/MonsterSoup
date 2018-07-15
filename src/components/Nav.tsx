34a25195f98d4bb2dd3ad620d45cd6bc80ef8123
Unknown <sbknick@gmail.com>
2018-07-15 22:02:19 +0100

    Added fake SRD Load menu option

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,7 +4,14 @@ export const Nav: React.StatelessComponent<{}> = () =>
 (
     <div className="navigation pure-menu pure-menu-horizontal pure-menu-fixed">
         <span className="pure-menu-heading">Monster Soup</span>
-        <nav />
+        <ul className="pure-menu-list">
+            <li className="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
+                <a href="#" id="menuLink1" className="pure-menu-link">Load Monster</a>
+                <ul className="pure-menu-children">
+                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">SRD</a></li>
+                </ul>
+            </li>
+        </ul>
     </div>
 );
 
