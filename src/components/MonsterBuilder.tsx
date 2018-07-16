2fbfaf1e43b856cdfbf1bea926d03a22956f75b7
Blair <b.knickerbocker@shieldsafety.co.uk>
2018-07-16 16:37:48 +0100

    Made routing to SRD load page work (needs refinement)

--- src/components/MonsterBuilder.tsx
+++ src/components/MonsterBuilder.tsx
@@ -1,4 +1,7 @@
 import * as React from "react";
+import { Route } from "react-router-dom";
+
+import LoadMonsterSRD from "src/components/loadMonster/LoadMonsterSRD";
 import MonsterStats from "src/components/monsterStats/MonsterStats";
 import Nav from "./Nav";
 
@@ -7,7 +10,9 @@ export const MonsterBuilder: React.StatelessComponent<{}> = () =>
 (
     <div>
         <Nav />
-        <MonsterStats />
+        {/* <Route path="" component={} /> */}
+        <Route path="/edit" component={MonsterStats} />
+        <Route path="/load/srd" component={LoadMonsterSRD} />
     </div>
 );
 
